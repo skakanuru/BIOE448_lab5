758f0004a9de4efa993e027b9f13a5a669cf320f
skakanuru <kakanuru.sathvik@gmail.com>
2024-02-18 23:12:55 -0600

    Replacing code with code from Canvas

--- bluetoothCode/bluetoothCode.ino
+++ bluetoothCode/bluetoothCode.ino
@@ -1,45 +1,81 @@
-const int trigPin = 11;
-const int echoPin = 12;
-const int redPin = 2;
-const int greenPin = 3;
-double duration;
-double distanceCm, distanceInch;
-double R1 = 0.01724;
-double R2 = 0.006329;
-double distanceLimit = 10;
+#include <ArduinoBLE.h>
+BLEService newService("180A");
+
+BLEByteCharacteristic readChar("2A58", BLERead);
+BLEByteCharacteristic writeChar("2A57", BLEWrite);
+
+long previousMillis = 0;
+
+const int trigPin = 8;
+const int echoPin = 10;
+long duration;
+int distanceCm, distanceInch;
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
   Serial.begin(9600);
+
+  while(!Serial);
+  if (!BLE.begin()){
+    Serial.println("Waiting for ArduinoBLE");
+    while(1);
+  }
+
+  BLE.setDeviceName("PaulaSathvik");
+  BLE.setAdvertisedService(newService);
+  newService.addCharacteristic(readChar);
+  newService.addCharacteristic(writeChar);
+  BLE.addService(newService);
+  
+  readChar.writeValue(0);
+  writeChar.writeValue(0);
+
+  BLE.advertise();
+  Serial.println("Bluetooth device active");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW); 
-  
-  duration = pulseIn(echoPin, HIGH);
-  distanceCm = duration * R1;
-  distanceInch = duration * R2;
-  Serial.print("Distance: ");
-  Serial.print(distanceCm);
-  Serial.print(" cm/");
-  Serial.print(distanceInch);
-  Serial.println(" in");
-  if (distanceCm < distanceLimit){
-    digitalWrite(greenPin, HIGH);
-    digitalWrite(redPin, LOW);
-  }
-  if (distanceCm > distanceLimit){
-      digitalWrite(redPin, HIGH);
-      digitalWrite(greenPin, LOW);
-  }
-  delay(100);
 
+  BLEDevice central = BLE.central(); // wait for a BLE central
+
+  if (central) {  // if a central is connected to the peripheral
+    Serial.print("Connected to central: ");
+    
+    Serial.println(central.address()); // print the central's BT address
+    
+    digitalWrite(LED_BUILTIN, HIGH); // turn on the LED to indicate the connection
+
+    while (central.connected()) { // while the central is connected:
+      long currentMillis = millis();
+      
+      if (currentMillis - previousMillis >= 200) { 
+        previousMillis = currentMillis;
+
+        digitalWrite(trigPin, LOW);
+        delayMicroseconds(2);
+        digitalWrite(trigPin, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trigPin, LOW);
+
+        duration = pulseIn(echoPin, HIGH);
+        distanceCm = duration * 0.034 / 2;
+
+        Serial.print("Distance: ");
+        Serial.print(distanceCm);
+        Serial.println(" cm");
+
+        delay(10);
+        if (writeChar.written()) {
+          if (writeChar.value()) {
+            readChar.writeValue(distanceCm);
+            Serial.println("Distance printed to peripheral");
+          }
+        }
+      }
+    }
+    
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
 }
